1a07631fe9cbb998364a6ae14fe296000870fbe5
Salah Fathy <SalahFathy@gmail.com>
2025-05-22 23:36:56 +0300

    add subapase auth

--- lib/utils/constants/constants.dart
+++ lib/utils/constants/constants.dart
@@ -1,8 +1,8 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class Constants {
-  static const supabaseUrl = "https://lrfgyfyjfeymqjqwuwhv.supabase.co";
+  static const supabaseUrl = "https://qakmymrmhpxcrkbpfiol.supabase.co";
   static const supabaseKey =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxyZmd5ZnlqZmV5bXFqcXd1d2h2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY4MTk1NzYsImV4cCI6MjA2MjM5NTU3Nn0.hwbiIHcLyeydDzrD6bFgQdoPdQZlJTBYC3PGW_6R0xI";
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFha215bXJtaHB4Y3JrYnBmaW9sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc5MDUzNDksImV4cCI6MjA2MzQ4MTM0OX0.MFxFH5zyyCLTZ3v5V5NB_urp1EgXhSD1zZJa_Jil_24";
   static final supabase = Supabase.instance.client;
 }
